f6a33e5c004edd5711a65c316972c20c386358ca
Federico Ramallo <framallo@gmail.com>
2011-08-19 00:05:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CventSample::Application.config.secret_token = 'f34f97f46574f409b1ad7d18d6121c1cf3997c9a53e5c86bfb972122f1b7de6ab877459a7f63338cffa9db0199a25d36d0004592752b31558ff1bb8fd682949f'
