ab6673a9c9f0da4715d61f77630f16b8a4e5c0df
Federico Ramallo <framallo@gmail.com>
2011-08-19 02:39:04 -0500

    moved from symbols to strings, since Hash object doesn't include extended rails methods

--- vendor/plugins/cvent/lib/cvent.rb
+++ vendor/plugins/cvent/lib/cvent.rb
@@ -4,23 +4,20 @@ require 'savon'
 # Client for the CVent 
 #
 class Cvent
+  attr_accessor :client, :ops
 
   def initialize(ops={})
     @ops = ops
     @client = Savon::Client.new do |wsdl, http, wsse|
-      wsdl.document = @ops[:wsdl]
+      wsdl.document = @ops['wsdl']
       yield http if block_given?
     end
   end
 
   def login
     @client.request(:wsdl, :login) do |soap| 
-      soap.body = { username: @ops[:login], password: @ops[:password] } 
+      soap.body = { account_number: @ops['account_number'], username: @ops['login'], password: @ops['password'] } 
     end
   end
 
-  def client
-    @client 
-  end
-    
 end
--- vendor/plugins/cvent/test/cvent_test.rb
+++ vendor/plugins/cvent/test/cvent_test.rb
@@ -6,7 +6,7 @@ ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
  
 class CventTest < Test::Unit::TestCase
   def setup
-    @config = YAML::load(IO.read(File.dirname(__FILE__) + '/cvent.yml'))[ENV['RAILS_ENV']]
+    @config = YAML::load_file(File.dirname(__FILE__) + '/cvent.yml')[ENV['RAILS_ENV']]
   end
 
   def teardown
@@ -18,15 +18,18 @@ class CventTest < Test::Unit::TestCase
     end
   end
 
-  def test_login
-    assert_nothing_raised do
-      @client = Cvent.new(@config)
-      puts @client.login
-    end
-  end
-
-  #def test_debug
-    #debugger
-    #a = 'a'
+  #def test_login
+    #assert_nothing_raised do
+      #@client = Cvent.new(@config)
+      #puts @client.login
+    #end
   #end
+
+  def test_debug
+    @client = Cvent.new(@config)
+    puts @client.client.wsdl.inspect
+    puts @client.client.http.inspect
+    debugger
+    a = 'a'
+  end
 end
